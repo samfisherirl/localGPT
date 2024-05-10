4bbaa6459b173f54a8fb58e99f2cdc2b682e3330
Sam A <98753696+samfisherirl@users.noreply.github.com>
2024-05-10 18:06:02 -0400

    __process

---
+++ one-click-installer.bat
@@ -0,0 +1,48 @@
+@echo off
+SET venv_dir=venv
+SET pyfile=main.py
+SET python=%venv_dir%\Scripts\python.exe
+set LLAMA_CUBLAS="1"
+set FORCE_CMAKE="1"
+set CMAKE_ARGS="-DLLAMA_CUBLAS=on"
+python -m pip install llama-cpp-python --prefer-binary --extra-index-url=https://jllllll.github.io/llama-cpp-python-cuBLAS-wheels/AVX2/cu117
+
+REM Check if the virtual environment directory exists
+IF EXIST "%venv_dir%\Scripts\activate.bat" (
+    ECHO Virtual environment found. Activating...
+    CALL %venv_dir%\Scripts\activate.bat
+) ELSE (
+    ECHO Creating virtual environment...
+    python -m venv %venv_dir%
+    CALL %venv_dir%\Scripts\activate.bat
+)
+
+REM Check if the virtual environment is activated
+IF NOT "%VIRTUAL_ENV%" == "" (
+    ECHO Virtual environment activated.
+    ECHO Installing dependencies...
+    %python% -m pip install --upgrade pip
+    %python% -m pip install -r requirements.txt
+    ECHO Dependencies installed.
+    
+    REM Check if the specified pyfile exists, if not, find the first .py file except __init__.py
+    IF NOT EXIST "%pyfile%" (
+        FOR /F "delims=" %%i IN ('DIR *.py /B /A:-D /O:N 2^>nul') DO (
+            IF NOT "%%i" == "__init__.py" (
+                SET "pyfile=%%i"
+                GOTO FoundPyFile
+            )
+        )
+        ECHO No suitable Python file found. Exiting...
+        GOTO End
+    )
+    :FoundPyFile
+    REM Run the Python script
+    %python% %pyfile%
+) ELSE (
+    ECHO Failed to activate virtual environment.
+)
+
+:End
+REM Pause the command window
+cmd /k 
---
+++ runner_ingest_and_chat.bat
@@ -0,0 +1,39 @@
+@echo off
+SET venv_dir=venv
+SET python=%venv_dir%\Scripts\python.exe
+
+REM Check if the virtual environment directory exists
+IF EXIST "%venv_dir%\Scripts\activate.bat" (
+    ECHO Virtual environment found. Activating...
+    CALL %venv_dir%\Scripts\activate.bat
+) ELSE (
+    ECHO Creating virtual environment...
+    python -m venv %venv_dir%
+    CALL %venv_dir%\Scripts\activate.bat
+)
+
+echo Please choose an option:
+echo 1. Run ingest.py
+echo 2. Run run_localGPT.py
+echo.
+
+choice /C 12 /N /M "Enter your choice (1-2):"
+
+if errorlevel 2 goto option2
+if errorlevel 1 goto option1
+
+:option1
+echo Running ingest.py with CUDA...
+python ingest.py --device_type cuda
+goto end
+
+:option2
+echo Running run_localGPT.py with CUDA...
+python run_localGPT.py --device_type cuda
+goto end
+
+:end
+echo Script finished.
+pause
+REM Pause the command window
+cmd /k 
